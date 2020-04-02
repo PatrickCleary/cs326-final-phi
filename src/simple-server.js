12d560fb16e21d4b8a5a9baee2fed171a607ccab
PatrickCleary <46229349+PatrickCleary@users.noreply.github.com>
2020-04-02 16:54:41 -0400

    wireframe done

--- src/simple-server.js
+++ src/simple-server.js
@@ -1,2 +1,3 @@
 'use strict';
-console.log('check 1 2');
+var express = require('express');
+var app = express();
--- src/simple-server.ts
+++ src/simple-server.ts
@@ -1,3 +1,3 @@
 'use strict';
-let express = require('express')
+let express = require('express');
 let app = express();
