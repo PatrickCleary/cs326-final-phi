41e8042b1420683e8d2979add50f1d063e26a988
PatrickCleary <46229349+PatrickCleary@users.noreply.github.com>
2020-04-23 21:47:01 -0400

    heroku3

--- src/database.js
+++ src/database.js
@@ -7,7 +7,7 @@ var Database = /** @class */ (function () {
     //Import the mongoose module
     //Set up default mongoose connection
     function Database(name) {
-        mongoose.connect(process.env.mongoDBuri, { useNewUrlParser: true });
+        mongoose.connect("mongodb+srv://heroku_user:potato99@cluster0-krl3x.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
         //Get the default connection
         var db = mongoose.connection;
         //Bind connection to error event (to get notification of connection errors)
--- src/database.ts
+++ src/database.ts
@@ -6,7 +6,7 @@ export class Database {
   //Import the mongoose module
   //Set up default mongoose connection
   constructor(name: String) {
-    mongoose.connect(process.env.mongoDBuri, { useNewUrlParser: true });
+    mongoose.connect("mongodb+srv://heroku_user:potato99@cluster0-krl3x.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
 
     //Get the default connection
     var db = mongoose.connection;
