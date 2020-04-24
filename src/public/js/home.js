f5d3db6769ecde861d13d675e95bf0539f5fa081
PatrickCleary <46229349+PatrickCleary@users.noreply.github.com>
2020-04-23 20:46:14 -0400

    heroku

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,3 +1,4 @@
+"use strict";
 //TODO: Fix double variable names, scope issue, url2/postData2/newURL2,data2
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
