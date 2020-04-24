f4507751b26fc01189a73fdcb73bd5a57bfbd16a
PatrickCleary <46229349+PatrickCleary@users.noreply.github.com>
2020-04-23 20:39:16 -0400

    patrick

--- src/models/User.js
+++ src/models/User.js
@@ -1,12 +1,11 @@
-"use strict";
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var UserSchema = new Schema({
     username: { type: String, required: true, max: 100 },
     email: { type: String, required: true, max: 100 },
     password: { type: String, required: true, max: 30 },
-    tested: { type: Boolean, default: false },
-    testedResult: { type: Number, default: -1, enum: [-1, 0, 1] },
+    tested: { type: Boolean, "default": false },
+    testedResult: { type: Number, "default": -1, "enum": [-1, 0, 1] },
     symptom: { type: Schema.Types.ObjectId, ref: 'Symptom' }
 });
 UserSchema
--- src/public/js/client.js
+++ src/public/js/client.js
@@ -35,7 +35,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 function postData(url, data) {
     return __awaiter(this, void 0, void 0, function () {
         var resp;
@@ -59,4 +59,4 @@ function postData(url, data) {
         });
     });
 }
-exports.default = postData;
+exports["default"] = postData;
