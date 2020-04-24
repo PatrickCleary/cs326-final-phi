ea9190027454a83f6439102027e1929be7338475
Nathan Duarte <nduartech101@gmail.com>
2020-04-23 23:05:15 -0400

    Ok

--- src/database.ts
+++ src/database.ts
@@ -6,7 +6,8 @@ export class Database {
   //Import the mongoose module
   //Set up default mongoose connection
   constructor(name: String) {
-    var mongoDB = 'mongodb+srv://heroku_user:potato99@cluster0-krl3x.coronaDB.net/test?retryWrites=true&w=majority';
+    //var mongoDB = 'mongodb+srv://heroku_user:potato99@cluster0-krl3x.coronaDB.net/test?retryWrites=true&w=majority';
+    var mongoDB = 'mongodb://127.0.0.1/' + name;
     mongoose.connect(mongoDB, { useNewUrlParser: true });
 
     //Get the default connection
--- src/routes/SymptomRouter.ts
+++ src/routes/SymptomRouter.ts
@@ -11,18 +11,19 @@ router.get('/:username/checkup',function(req:any,res:any) {
 
 router.post('/:username/update', async (req:any, res:any) => {
   req.body.username = req.params.username;
-  let curr_user = await User.findOne({username: req.params.username}, ['_id', 'tested', 'testedResult'],  { lean: true },
+  var curr_user = await User.findOne({username: req.params.username}, ['_id', 'tested', 'testedResult'],  { lean: true },
   function(err:any,u:any) {
     return u;
   });
-  var sick = new Symptom({user: curr_user._id, fever: req.body.Symptoms.fever, tiredness: req.body.Symptoms.tiredness,chills: req.body.Symptoms.chills, digestion: req.body.Symptoms.digestion, smell: req.body.Symptoms.smell, congestion: req.body.Symptoms.congestion, cough: req.body.Symptoms.cough, breathing: req.body.Symptoms.breathing});
+  var sick = new Symptom({user: curr_user._id, fever: req.body.fever, tiredness: req.body.tiredness,chills: req.body.chills, digestion: req.body.digestion, smell: req.body.smell, congestion: req.body.congestion, cough: req.body.cough, breathing: req.body.breathing});
   sick.save(function(err:any) {
     if (err) return err;
     User.findOneAndUpdate({_id: curr_user._id}, {symptom:sick._id, tested:req.body.tested,testedResult:req.body.testedResult},function(err:any) {
       if (err) return err;
-
+      else {return 0;}
     });
   });
+  res.redirect('/');
 });
 
 module.exports = router;
