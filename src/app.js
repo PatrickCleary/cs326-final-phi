0e07c2b048e235bc322d9f092a72e23e1ab84bcc
stephendisario <sdisario@umass.edu>
2020-04-24 21:23:03 -0400

    minor

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var database_1 = require("./database");
 var server_1 = require("./server");
-var database = new database_1.Database('test');
+var database = new database_1.Database('coronaDB');
 var server = new server_1.Server(database);
 server.listen(8080);
